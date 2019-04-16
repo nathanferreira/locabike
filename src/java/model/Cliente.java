97f986d4cc6e754f9e5e5d78c616ddf5ab11f7ef
tretinha <gustavogiordanocolombini@gmail.com>
2019-04-16 10:07:57 -0300

    Add "Cliente" model with getters and setters

--- src/java/model/Cliente.java
+++ src/java/model/Cliente.java
@@ -1,14 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
-/**
- *
- * @author 587877
- */
 public class Cliente {
+    private String email;
+    private String password;
+    private String CPF;
+    private String name;
+    private String gender;
+    private String birthDate;
     
+    public Cliente(String email, String password, String CPF, String name,
+            String gender, String birthDate) {
+        this.email = email;
+        this.password = password;
+        this.CPF = CPF;
+        this.name = name;
+        this.gender = gender;
+        this.birthDate = birthDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
 }
